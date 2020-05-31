2a0ba10dfd30f2daad2d06365a3771076bf04c78
Royce <royce963@gmail.com>
2020-05-31 13:55:31 +0800

    some changes

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,22 @@ const bodyParser = require('body-parser');
 // to be requested from another domain outside the domain from which the first resource was served.
 const cors = require('cors'); 
 
+
 // A Node.js framework which lets us access MongoDB in an object-oriented way.
 const mongoose = require('mongoose'); 
+const connection = "mongodb+srv://User0:Y9juVI7j5pq1spW9@cluster0-asita.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
+// ------------------------------------------------------------------------
 
 const path = require('path'); // needed for deployment/production
 const app = express(); // initialize express??
 
+// --------------------------------------------------------------------
 app.get('/', (req, res) => res.send('Hello world!')); // http://localhost:4000/ will show the text here.
 
+// --------------------------------------------------------------------
 const port = process.env.PORT || 4000; // Decides which port the server is running on
 
 app.listen(port, () => console.log(`Server running on port ${port}`)); // Prints out port number in console
